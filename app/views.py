f7d0ae496ecebf26fe718173d0c1266f2a11b691
seantg <seantg@vt.edu>
2016-03-25 12:02:59 -0400

    prototype pages created

--- app/views.py
+++ app/views.py
@@ -1,7 +1,15 @@
-from flask import render_template
+from flask import render_template, Flask, request, url_for, redirect
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
 	return render_template('index.html')
+
+@app.route('/reflection')
+def reflection():
+    return render_template('reflection.html')
+
+@app.route('/collection')
+def collection():
+    return render_template('collection.html')
